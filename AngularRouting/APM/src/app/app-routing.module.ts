dc05def3998f2ad1315011832cb2fa097384a488
DevinEllingson <devin5885@gmail.com>
2021-02-23 21:30:37 -0800

    Angular Routing - Completed (12 - Lazy Loading)

--- AngularRouting/APM/src/app/app-routing.module.ts
+++ AngularRouting/APM/src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthGuard } from './user/auth.guard';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      {
+        path: 'products',
+        canActivate: [AuthGuard],
+        data: { preload: false },
+        loadChildren: () =>
+          import('./products/product.module').then(m => m.ProductModule)
+      },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent }
-    ]),
+    ], { preloadingStrategy: SelectiveStrategy}),
   ],
   declarations: [
   ],
